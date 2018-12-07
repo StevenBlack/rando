ccf546c7982e0dccbdc1204ca8214de89a850846
Steven Black <steveb@stevenblack.com>
2018-12-07 01:08:17 -0500

    Add a flag for number of times to repeat generation.

--- rando.go
+++ rando.go
@@ -14,13 +14,14 @@ import (
 
 func main() {
 	var seed int64
-	var rando, pow, num, maxrand, cols, max int
+	var rando, pow, num, maxrand, cols, max, times int
 	var uniq, sort bool
 
 	flag.IntVar(&pow, "l", 8, "The length, in powers of 10, of the random numbers")
 	flag.IntVar(&num, "n", 1, "The number of random numbers to generate")
 	flag.IntVar(&cols, "c", 1, "The number of columns to generate")
 	flag.IntVar(&max, "m", 0, "The upper limit of the random numbers to generate")
+	flag.IntVar(&times, "t", 1, "The number of times to generate the random numbers")
 	flag.BoolVar(&sort, "s", false, "Sort the random numbers?")
 	flag.BoolVar(&uniq, "u", false, "Should the random numbers be unique?")
 	flag.Parse()
@@ -33,30 +34,32 @@ func main() {
 		maxrand = max
 	}
 
-	list := arraylist.New()
-	for i := 0; i < num; i++ {
-		rando = rand.Intn(maxrand) + 1
-		if uniq {
-			if ! list.Contains(rando) {
-				list.Add(rando)
+	for t := 0; t < times; t++ {
+		list := arraylist.New()
+		for i := 0; i < num; i++ {
+			rando = rand.Intn(maxrand) + 1
+			if uniq {
+				if ! list.Contains(rando) {
+					list.Add(rando)
+				} else {
+					i--
+				}
 			} else {
-				i--
+				list.Add(rando)
 			}
-		} else {
-			list.Add(rando)
 		}
-	}
 
-	if sort {
-		list.Sort(utils.IntComparator)
-	}
+		if sort {
+			list.Sort(utils.IntComparator)
+		}
 
-	for i := 0; i < num; i++ {
-		// B % A gives 0
-		a, _ := list.Get(i)
-		fmt.Printf("%"+strconv.Itoa(pow+1)+"d", a)
-		if (i+1)%cols == 0 {
-			fmt.Printf("\n")
+		for i := 0; i < num; i++ {
+			// B % A gives 0
+			a, _ := list.Get(i)
+			fmt.Printf("%"+strconv.Itoa(pow+1)+"d", a)
+			if (i+1)%cols == 0 {
+				fmt.Printf("\n")
+			}
 		}
 	}
 }
