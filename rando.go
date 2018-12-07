386072247fdde6903b48a7e8f7f617c9d4aae41d
Steven Black <steveb@stevenblack.com>
2018-12-07 00:47:15 -0500

    Add flag for unique randoms.

--- rando.go
+++ rando.go
@@ -13,11 +13,13 @@ import (
 func main() {
 	var seed int64
 	var rando, pow, num, maxrand, cols, max int
+	var uniq bool
 
 	flag.IntVar(&pow, "l", 8, "The length, in powers of 10, of the random numbers")
 	flag.IntVar(&num, "n", 1, "The number of random numbers to generate")
 	flag.IntVar(&cols, "c", 1, "The number of columns to generate")
 	flag.IntVar(&max, "m", 0, "The upper limit of the random numbers to generate")
+	flag.BoolVar(&uniq, "u", false, "Should the random numbers be unique?")
 	flag.Parse()
 
 	seed = time.Now().UnixNano()
@@ -31,7 +33,15 @@ func main() {
 	list := arraylist.New()
 	for i := 0; i < num; i++ {
 		rando = rand.Intn(maxrand) + 1
-		list.Add(rando)
+		if uniq {
+			if ! list.Contains(rando) {
+				list.Add(rando)
+			} else {
+				i--
+			}
+		} else {
+			list.Add(rando)
+		}
 	}
 
 	for i := 0; i < num; i++ {
