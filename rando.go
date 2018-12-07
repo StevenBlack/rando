9605e23d05bc43b5aa9c40a33e112e363e7f4ec2
Steven Black <steveb@stevenblack.com>
2018-12-07 01:09:14 -0500

    Sort the flag statements so they are easier to grok.

--- rando.go
+++ rando.go
@@ -17,13 +17,13 @@ func main() {
 	var rando, pow, num, maxrand, cols, max, times int
 	var uniq, sort bool
 
-	flag.IntVar(&pow, "l", 8, "The length, in powers of 10, of the random numbers")
-	flag.IntVar(&num, "n", 1, "The number of random numbers to generate")
-	flag.IntVar(&cols, "c", 1, "The number of columns to generate")
-	flag.IntVar(&max, "m", 0, "The upper limit of the random numbers to generate")
-	flag.IntVar(&times, "t", 1, "The number of times to generate the random numbers")
 	flag.BoolVar(&sort, "s", false, "Sort the random numbers?")
 	flag.BoolVar(&uniq, "u", false, "Should the random numbers be unique?")
+	flag.IntVar(&cols, "c", 1, "The number of columns to generate")
+	flag.IntVar(&max, "m", 0, "The upper limit of the random numbers to generate")
+	flag.IntVar(&num, "n", 1, "The number of random numbers to generate")
+	flag.IntVar(&pow, "l", 8, "The length, in powers of 10, of the random numbers")
+	flag.IntVar(&times, "t", 1, "The number of times to generate the random numbers")
 	flag.Parse()
 
 	seed = time.Now().UnixNano()
