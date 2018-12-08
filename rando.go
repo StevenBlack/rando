7ab68291f194ef52325181fb295f13723ba8bfcb
Steven Black <steveb@stevenblack.com>
2018-12-08 00:43:03 -0500

    Add long forms of the config flags.

--- rando.go
+++ rando.go
@@ -7,28 +7,43 @@ import (
 	"math/rand"
 	"strconv"
 	"time"
+
 	"github.com/emirpasic/gods/lists/arraylist"
 	"github.com/emirpasic/gods/utils"
 )
 
 func main() {
 	var seed int64
-	var rando, pow, num, maxrand, cols, max, times int
+	var rando, digits, num, maxrand, cols, max, times int
 	var uniq, sort bool
 
 	flag.BoolVar(&sort, "s", false, "Sort the random numbers?")
+	flag.BoolVar(&sort, "sort", false, "Sort the random numbers?")
+
 	flag.BoolVar(&uniq, "u", false, "Should the random numbers be unique?")
+	flag.BoolVar(&uniq, "unique", false, "Should the random numbers be unique?")
+
 	flag.IntVar(&cols, "c", 1, "The number of columns to generate")
+	flag.IntVar(&cols, "columns", 1, "The number of columns to generate")
+
+	flag.IntVar(&digits, "d", 8, "The number of digits in the random numbers")
+	flag.IntVar(&digits, "digits", 8, "The number of digits in the random numbers")
+
 	flag.IntVar(&max, "m", 0, "The upper limit of the random numbers to generate")
+	flag.IntVar(&max, "max", 0, "The upper limit of the random numbers to generate")
+
 	flag.IntVar(&num, "n", 1, "The number of random numbers to generate")
-	flag.IntVar(&pow, "l", 8, "The length, in powers of 10, of the random numbers")
+	flag.IntVar(&num, "number", 1, "The number of random numbers to generate")
+
 	flag.IntVar(&times, "t", 1, "The number of times to generate the random numbers")
+	flag.IntVar(&times, "times", 1, "The number of times to generate the random numbers")
+
 	flag.Parse()
 
 	seed = time.Now().UnixNano()
 	rand.Seed(seed)
 	if max == 0 {
-		maxrand = int(math.Pow10(pow))
+		maxrand = int(math.Pow10(digits))
 	} else {
 		maxrand = max
 	}
@@ -38,7 +53,7 @@ func main() {
 		for i := 0; i < num; i++ {
 			rando = rand.Intn(maxrand) + 1
 			if uniq {
-				if ! list.Contains(rando) {
+				if !list.Contains(rando) {
 					list.Add(rando)
 				} else {
 					i--
@@ -55,7 +70,7 @@ func main() {
 		for i := 0; i < num; i++ {
 			// B % A gives 0
 			a, _ := list.Get(i)
-			fmt.Printf("%"+strconv.Itoa(pow+1)+"d", a)
+			fmt.Printf("%"+strconv.Itoa(digits+1)+"d", a)
 			if (i+1)%cols == 0 {
 				fmt.Printf("\n")
 			}
